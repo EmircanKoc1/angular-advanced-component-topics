8cff0acaff4c969b53938b91484d069deb889039
Emircan <emircandv787@gmail.com>
2024-10-31 11:05:15 +0300

    The ng container example has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./ng-content/card/card.component";
+import { CommonModule } from '@angular/common';
+import { SimpleComponentComponent } from './ng-container/simple-component/simple-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent,CommonModule,SimpleComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
